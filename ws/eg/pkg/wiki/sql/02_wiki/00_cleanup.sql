f524ea7ecbe04444cb579730de682808b13ade36
Alexey Kovrizhkin <jean@tender.pro>
2012-07-04 21:18:53 +0400

    added class cleanup

--- ws/eg/pkg/wiki/sql/02_wiki/00_cleanup.sql
+++ ws/eg/pkg/wiki/sql/02_wiki/00_cleanup.sql
@@ -12,6 +12,9 @@ DELETE FROM ws.page_data             WHERE code LIKE 'wiki%' OR up_code='wiki'; 
 DELETE FROM ws.method           WHERE code_real LIKE 'wiki.%' OR code_real LIKE 'Wiki::%';
 --class_id IN (6,7,8) OR code ~ E'(tmpl|tender|offer)\.';
 
+DELETE FROM ws.class_action WHERE class_id IN (5);
+DELETE FROM ws.class_status WHERE class_id IN (5);
+DELETE FROM ws.class WHERE id IN (5);
 
 -- pg_cs('z_wikiformat')
 DELETE FROM ws.dt_part          WHERE id IN (SELECT id FROM ws.dt WHERE code LIKE 'wiki.%');
@@ -22,4 +25,4 @@ DELETE from ws.dt               WHERE code LIKE 'wiki.%';
 DELETE FROM ws.error_data WHERE code ~ E'^Y99';
 
 /* ------------------------------------------------------------------------- */
-\qecho '-- FD: wiki:wiki:00_cleanup.sql / 25 --'
+\qecho '-- FD: wiki:wiki:00_cleanup.sql / 28 --'
